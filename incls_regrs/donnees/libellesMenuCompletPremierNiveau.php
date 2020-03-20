733f17c80a3df036a1e8f5f6ef58942316f0ec29
Kyzaro YTB <wizkilleur@gmail.com>
2020-03-20 14:16:49 +0100

    Fichier php Include fini

---
+++ incls_regrs/donnees/libellesMenuCompletPremierNiveau.php
@@ -0,0 +1,6 @@
+<?php
+// Fichier libellesMenuCompletPremierNiveau.php
+// Premier niveau du menu
+$chaineMenu="Accueil:Modules Informatiques du BTS SIO:Calculs binaires et conversions:Calculs IP:Concevoir un site:Développement Web:Aspect Graphique:À Propos";
+return $chaineMenu;
+?>
--- index.php
+++ index.php
@@ -1,36 +1,15 @@
 <?php
 
+ $laVariable = require_once 'incls_regrs/donnees/libellesMenuCompletPremierNiveau.php';
+
 ?>
+
 <!DOCTYPE html>
 <html>
 	<?php
-				include 'incls_regrs/donnees/head.html'
-			?>
-	<body>
-		<header>
-			<h1>Mon application DEROO Dylan</h1>
-			
-			<div>
-			<!--Authentification (nom et mdp)-->
-					
-			</div>
-			<div>
-				<img			/>
-			</div>
-			<?php
-				include 'incls_regrs/donnees/nav.html'
-			?>
-		</header>
-			<main>
-				<aside>
-					
-				</aside>
-				<article>
-				
-				</article>
-			</main>
-		<footer>
-		
-		</footer>
-	</body>
+		include 'incls_regrs/donnees/head.html';
+	?>
+	<?php
+		include 'incls_regrs/donnees/body.html' 
+	?>
 </html>
