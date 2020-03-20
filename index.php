e3af933b1f3dfe8ef3bb15b2efa9293e0107f16f
Kyzaro YTB <wizkilleur@gmail.com>
2020-03-20 10:31:39 +0100

    Le fichier que en HTML

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Index</title>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" type="text/css" title="CSS" href="style/stylesDuMenu.css"/>
+	</head>
+	<body>
+		<header>
+			<h1>Mon application DEROO Dylan</h1>
+			
+			<div>
+			<!--Authentification (nom et mdp)-->
+					
+			</div>
+			<div>
+				<img			/>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Web Design</a>
+				<ul>
+					<li><a href="#">HTML</a></li>
+					<li><a href="#">CSS</a></li>
+					<li><a href="#">JavaScript</a></li>
+				</ul>        
+					</li>
+					<li><a href="#">Web Development</a>
+				<ul>
+					<li><a href="#">PHP</a></li>
+					<li><a href="#">Node JS</a></li>
+					<li><a href="#">Advance JS</a>
+				<ul>
+					<li><a href="#">Angular</a></li>
+					<li><a href="#">VUE</a></li>
+					<li><a href="#">React</a>
+				<ul>
+					<li><a href="#">React Native</a></li>
+					<li><a href="#">React JS</a></li>
+					<li><a href="#">Material Design</a></li>
+				</ul>
+					</li>
+				</ul>
+					</li>
+				</ul>
+					</li>
+					<li><a href="#">Graphic Design</a>
+			   <ul>
+					<li><a href="#">Photoshop</a></li>
+					<li><a href="#">Illustrator</a></li>
+					<li><a href="#">Adobe XD</a></li>
+				</ul>  
+					</li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">About</a></li>
+				</ul>
+			</nav>
+		</header>
+			<main>
+				<aside>
+					
+				</aside>
+				<article>
+				
+				</article>
+			</main>
+		<footer>
+		
+		</footer>
+	</body>
+</html>
